3e3639c7760663329013ea1fee2a78311014b164
leiwuyi <leiwuyi51@qq.com>
2019-08-04 00:24:51 +0800

    卖座网RN

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>welcome maizuo!</Text>
+        
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+
+});
